a96b5cfd333df5dd4b98608dcbed7892fabb0c94
MMgdh <976983151@qq.com>
2021-11-16 21:15:50 +0800

    CSS

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyStudy_HtmlCssJavaScript.Controllers
     {
         public IActionResult Index()
         {
-            return View("Html5_svg");
+            return View("Index_CSS");
         }
 
         public IActionResult Test()
