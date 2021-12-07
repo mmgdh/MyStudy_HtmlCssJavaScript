b537dcc3e62e0561954478d96c0f5e0ed9e5dea4
林庆汉 <mmgdh@sina.com>
2021-12-07 17:33:31 +0800

    推送

--- Controllers/JavaScriptController.cs
+++ Controllers/JavaScriptController.cs
@@ -10,7 +10,7 @@ namespace MyStudy_HtmlCssJavaScript.Controllers
     {
         [Route("")]
         [Route("Index")]
-        public IActionResult Array()
+        public IActionResult TypedArray()
         {
             return View();
         }
